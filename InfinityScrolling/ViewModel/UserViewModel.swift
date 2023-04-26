38e45ed74e1211e57f3cacc2e36682d212d911e7
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-26 15:04:32 +0530

    define published and private variable connect with api and add switch case for loading printed

---
+++ InfinityScrolling/ViewModel/UserViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  UserViewModel.swift
+//  InfinityScrolling
+//
+//  Created by Bhumika Patel on 26/04/23.
+//
+
+import Foundation
+import Combine
+
+class UserViewModel: ObservableObject{
+    @Published var users: [User] = []
+    @Published var isRequestFailed = false
+    private let pageLimit = 10
+    private var currentLastId: Int? = nil
+    private var cancellable: AnyCancellable?
+    
+    func getUsers() {
+        cancellable = ApiService.shared.getUsers(perPage: pageLimit, sinceId: currentLastId)
+            .receive(on: DispatchQueue.main)
+            .sink{ completion in
+                switch completion {
+                case .failure(let error) :
+                    self.isRequestFailed = true
+                    print(error)
+                case .finished:
+                   print("")
+                }
+            } receiveValue: { users in
+                self.users.append(contentsOf: users)
+                self.currentLastId = users.last?.id
+            }
+    }
+}
