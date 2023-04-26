2fb4ba476c4f94df6d7b25567b7e8da716de31d6
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-26 16:37:39 +0530

    change api name and add print of finished loading

--- InfinityScrolling/ViewModel/UserViewModel.swift
+++ InfinityScrolling/ViewModel/UserViewModel.swift
@@ -8,27 +8,28 @@
 import Foundation
 import Combine
 
-class UserViewModel: ObservableObject{
+class UserViewModel: ObservableObject {
     @Published var users: [User] = []
     @Published var isRequestFailed = false
     private let pageLimit = 10
     private var currentLastId: Int? = nil
     private var cancellable: AnyCancellable?
-    
+     
     func getUsers() {
-        cancellable = ApiService.shared.getUsers(perPage: pageLimit, sinceId: currentLastId)
+        cancellable = APIService.shared.getUsers(perPage: pageLimit, sinceId: currentLastId)
             .receive(on: DispatchQueue.main)
-            .sink{ completion in
+            .sink { completion in
                 switch completion {
-                case .failure(let error) :
+                case .failure(let error):
                     self.isRequestFailed = true
                     print(error)
                 case .finished:
-                   print("")
+                    print("finished loading")
                 }
             } receiveValue: { users in
                 self.users.append(contentsOf: users)
                 self.currentLastId = users.last?.id
             }
+ 
     }
 }
