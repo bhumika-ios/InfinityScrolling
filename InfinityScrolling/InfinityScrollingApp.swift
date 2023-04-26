da5a0b99c1a2600d68675b0d39ed3b3deb2b256a
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-26 13:32:31 +0530

    Initial Commit

---
+++ InfinityScrolling/InfinityScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinityScrollingApp.swift
+//  InfinityScrolling
+//
+//  Created by Bhumika Patel on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct InfinityScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
