e8eb3df2f648531b100ccc81f7b204f80618445a
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-26 16:35:11 +0530

    change name of struct

--- InfinityScrolling/ApiManager/ApiService.swift
+++ InfinityScrolling/ApiManager/ApiService.swift
@@ -7,21 +7,20 @@
 
 import Foundation
 import Combine
-
-class ApiService {
-     static let shared = ApiService()
-    
+ 
+class APIService {
+    static let shared = APIService()
     func getUsers(perPage: Int = 30, sinceId: Int? = nil) -> AnyPublisher<[User], Error> {
-        var components = URLComponents(string:"https://api.github.com/users")!
+        var components = URLComponents(string: "https://api.github.com/users")!
         components.queryItems = [
-                                URLQueryItem(name: "per_page", value: "\(perPage)"),
-                                URLQueryItem(name: "since", value: (sinceId != nil) ? "\(sinceId!)" : nil)
-                                ]
-                                             
-                            let request = URLRequest(url: components.url!, timeoutInterval: 5)
-                            return URLSession.shared.dataTaskPublisher(for: request)
-                                .map(\.data)
-                                .decode(type: [User].self, decoder: JSONDecoder())
-                                .eraseToAnyPublisher()
+            URLQueryItem(name: "per_page", value: "\(perPage)"),
+            URLQueryItem(name: "since", value: (sinceId != nil) ? "\(sinceId!)" : nil)
+        ]
+         
+        let request = URLRequest(url: components.url!, timeoutInterval: 5)
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map(\.data)
+            .decode(type: [User].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
     }
 }
