a89f54865096801991e5cc04f9cccf3f115352b6
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-27 17:23:49 +0530

    add navigation view for title

--- InfinityScrolling/View/ContentView.swift
+++ InfinityScrolling/View/ContentView.swift
@@ -14,23 +14,23 @@ struct ContentView: View {
     @State var columns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 2)
     
     var body: some View {
-
+        NavigationView{
             ScrollView(.vertical, showsIndicators: false){
-                HStack{
-                    Text("User List")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Spacer()
-                    Button{
-                        
-                    }label: {
-                        Image(systemName: "rectangle.grid.1x2")
-                            .font(.system(size: 24))
-                            .foregroundColor(.black)
-                    }
-                }
-                .padding(.horizontal)
-                .padding(.top,25)
+//                HStack{
+//                    Text("User List")
+//                        .font(.title)
+//                        .fontWeight(.bold)
+//                    Spacer()
+//                    Button{
+//
+//                    }label: {
+//                        Image(systemName: "rectangle.grid.1x2")
+//                            .font(.system(size: 24))
+//                            .foregroundColor(.black)
+//                    }
+//                }
+//                .padding(.horizontal)
+//                .padding(.top,25)
                 
                 LazyVGrid(columns: self.columns, spacing: 25){
                     ForEach(userVM.users, id: \.id) { user in
@@ -42,9 +42,12 @@ struct ContentView: View {
                 }
                 .padding([.horizontal, .top])
             }
-    
+            
             .background(Color.black.opacity(0.05).edgesIgnoringSafeArea(.all))
+            .navigationTitle("UserList")
+        }
     }
+     
     private func fetchData(){
         userVM.getUsers()
     }
