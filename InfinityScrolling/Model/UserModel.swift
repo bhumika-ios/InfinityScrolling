6f2b706e8c5ad841e946aeef5278a73492e76930
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-04-27 17:21:41 +0530

    key and value add in user model

--- InfinityScrolling/Model/UserModel.swift
+++ InfinityScrolling/Model/UserModel.swift
@@ -11,10 +11,12 @@ struct User: Decodable, Identifiable {
     let id: Int
     let name: String
     let avatarUrl: String
+    let htmlUrl: String
      
     enum CodingKeys: String, CodingKey {
         case id
         case name = "login"
         case avatarUrl = "avatar_url"
+        case htmlUrl = "html_url"
     }
 }
